41bec8d439a1412f2d0874053825d0219111b7a1
Adriano Cucci <cucciadriano@gmail.com>
2021-04-09 19:29:33 -0400

    Added new Spotify track.

--- src/app/services/spotify-urls.service.ts
+++ src/app/services/spotify-urls.service.ts
@@ -9,6 +9,11 @@ const IMG_SRC_FOLDER: string = "assets/images/album-covers";
 export class SpotifySourcesService {
   public readonly sources: SpotifySource[] = [
     {
+      url: "https://open.spotify.com/embed/track/6PogkqYpaWV2bW1pVRNs8s",
+      title: "All Night Long",
+      imageUrl: `${IMG_SRC_FOLDER}/all-night-long.jpg`
+    },
+    {
       url: "https://open.spotify.com/embed/track/1WSvbMs1Q1JFLGsyvAmZDa",
       title: "This Love",
       imageUrl: `${IMG_SRC_FOLDER}/this-love.jpg`
@@ -17,7 +22,7 @@ export class SpotifySourcesService {
       url: "https://open.spotify.com/embed/track/4PjA8d9XMZvyvlK4gf1amF",
       title: "Don't Wanna Be",
       imageUrl: `${IMG_SRC_FOLDER}/dont-wanna-be.jpg`
-    },
+    }
   ];
 
   constructor(sanitizer: DomSanitizer) {
