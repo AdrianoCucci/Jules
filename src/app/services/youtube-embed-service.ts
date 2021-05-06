50ef1249e5a3d6f617e5f1e60a92287ba64e0874
Adriano Cucci <cucciadriano@gmail.com>
2021-05-05 21:43:09 -0400

    Added new YouTube video to YouTubeEmbedService.

--- src/app/services/youtube-embed-service.ts
+++ src/app/services/youtube-embed-service.ts
@@ -6,6 +6,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class YouTubeEmbedService {
   public readonly sources: string[] | SafeResourceUrl[] = [
+    "https://www.youtube.com/embed/ZtCZfWioDrY",
     "https://www.youtube.com/embed/AknXzVv61MY",
     "https://www.youtube.com/embed/WnGPKwgSJJg",
     "https://www.youtube.com/embed/LRTeD0X4vU0"
